70943d635fbc809efe617e7117fde123e9ec3c0b
Alish <alish14.mod@gmail.com>
2026-02-09 03:17:32 -0500

    ehance error handling in phase1 and phase1 almost done

--- src/elf_parser.c
+++ src/elf_parser.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <elf.h>
+#include "elf.h"
 #include "elf_parser.h"
 
 int get_magic_number(unsigned char *magic_chars,FILE* file_ptr){
-    fread(magic_chars,sizeof(char),EI_NIDENT,file_ptr);
+    size_t n=fread(magic_chars,sizeof(unsigned char),EI_NIDENT,file_ptr);
+    if( n != EI_NIDENT ){
+        return -1;
+    }
+
     if(magic_chars[0] != ELF_MAGIC_0 ||
        magic_chars[1] != ELF_MAGIC_1 ||
        magic_chars[2] != ELF_MAGIC_2 ||
        magic_chars[3] != ELF_MAGIC_3
     ){
-        fclose(file_ptr);
         return -1;
     }
     return 0;
@@ -41,19 +44,25 @@ int get_elf_format(unsigned char *magic_chars,Class *elf){
 void parse_elf(const char *filename)
 {
     Class elf_format;
+    unsigned char e_ident[EI_NIDENT];
+
     FILE *fptr=fopen(filename,"rb");
     if(!fptr){
         printf("fopen");
         return;
     }
-    unsigned char e_ident[EI_NIDENT];
+
     if(get_magic_number(e_ident,fptr)<0){
         printf("Not an ELF file \n");
+        fclose(fptr);
         return;
     }
     printf("ELF file detected!\n");
+
     if(get_elf_format(e_ident,&elf_format) < 0){
         printf("error on detecting elf format closing peacefully");
+        fclose(fptr);
         return;
     }
+    fclose(fptr);
 }
