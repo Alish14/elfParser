18d990bd53fc81c5c93e2cc7693ede1dc8007d29
Alish <alish14.mod@gmail.com>
2026-02-09 03:17:32 -0500

    ehance error handling in phase1 and phase1 almost done

---
+++ include/elf.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define ELF_MAGIC_0 0x7f
+#define ELF_MAGIC_1 'E'
+#define ELF_MAGIC_2 'L'
+#define ELF_MAGIC_3 'F'
+#define ELF_EICLASS_INDEX 4
--- include/elf_parser.h
+++ include/elf_parser.h
@@ -3,12 +3,6 @@
 
 #include <stdint.h>
 
-#define ELF_MAGIC_0 0x7f
-#define ELF_MAGIC_1 'E'
-#define ELF_MAGIC_2 'L'
-#define ELF_MAGIC_3 'F'
-#define ELF_EICLASS_INDEX 4
-
 typedef enum{
     ELF_CLASS_NONE,
     ELF_CLASS_32,
