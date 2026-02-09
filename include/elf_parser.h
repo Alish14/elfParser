0c3b44a8ae22a00a9d93ce565ee7a76c4eb22fb2
Alish <alish14.mod@gmail.com>
2026-02-09 05:39:06 -0500

    refactor phase1 and extract all data from ident

--- include/elf_parser.h
+++ include/elf_parser.h
@@ -2,12 +2,32 @@
 #define ELF_PARSER_H
 
 #include <stdint.h>
+#include "elf.h"
+
 
 typedef enum{
-    ELF_CLASS_NONE,
-    ELF_CLASS_32,
-    ELF_CLASS_64
-}Class;
+    ELF_CLASS_NONE = 0,
+    ELF_CLASS_32 = 1,
+    ELF_CLASS_64 = 2
+}ElfClass;
+
+typedef enum {
+    ELF_DATA_NONE = 0,
+    ELF_DATA_LSB  = 1,
+    ELF_DATA_MSB  = 2
+} ElfData;
+
+
+typedef struct{
+    unsigned char    e_ident[EI_NIDENT]; /* raw bytes */
+    ElfClass elf_class;
+    ElfData  elf_data;
+    
+    unsigned char version;
+    unsigned char osabi;
+    unsigned char abi_version;
+
+}ElfIdent;
 
 void parse_elf(const char *filename);
 
