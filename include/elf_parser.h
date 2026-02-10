6bbd812176f9c9b57020aa9b873654c251cac138
Alish <alish14.mod@gmail.com>
2026-02-10 16:24:11 +0330

    End of phase2

--- include/elf_parser.h
+++ include/elf_parser.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include "elf.h"
 
-/**
-* ident parser structures
-*/
+/************************************************
+*           ident parser structures
+************************************************/
 typedef enum{
     ELF_CLASS_NONE = 0,
     ELF_CLASS_32 = 1,
@@ -31,14 +31,65 @@ typedef struct{
 
 }ElfIdent;
 
-/**
-* ident parser functions
-*/
-void parse_ident_elf(const char *filename);
+/**********************************************
+*           ident parser functions            *
+**********************************************/
 int get_magic_number(unsigned char *magic_chars,FILE* file_ptr);
 int get_elf_format(unsigned char *magic_chars,ElfIdent *elf);
 int get_ident_data_format(unsigned char *magic_chars,ElfIdent *elf);
 void get_ident_other_data(unsigned char *magic_chars,ElfIdent *elf);
 void get_ident_dump(unsigned char *magic_chars);
-void parse_ident_elf(const char *filename);
+void parse_ident_elf(const char *filename,ElfIdent *eident_values);
+
+/* =========================
+ * ELF file type
+ * ========================= */
+typedef enum{
+    ET_NONE=0,
+    ET_REL=1,
+    ET_EXEC=2,
+    ET_DYN=3,
+    ET_CORE=4,
+}ElfType;
+
+typedef enum {
+    ARCH_UNKNOWN = 0,
+    ARCH_X86     = 3,    // EM_386
+    ARCH_X86_64  = 62,   // EM_X86_64
+    ARCH_ARM     = 40,   // EM_ARM
+    ARCH_ARM64   = 183,  // EM_AARCH64
+    ARCH_MIPS    = 8,
+    ARCH_RISCV   = 243
+} ElfArch;
+
+/* =========================
+ *          ELF Header
+ * ========================= */
+typedef struct
+{
+    ElfIdent   ident;
+    ElfType    type;
+    ElfArch    arch;
+    uint32_t   version;
+    uint64_t   entry;
+    uint64_t   phoff;
+    uint64_t   shoff;
+    uint32_t   flags;
+    uint16_t   ehsize;
+    uint16_t   phentsize;
+    uint16_t   phnum;
+    uint16_t   shentsize;
+    uint16_t   shnum;
+    uint16_t   shstrndx;
+}ElfHeader;
+
+/* =========================
+ * ELF Header API
+ * ========================= 
+ * */
+
+void parse_elf_header(const char *filename, ElfHeader *header);
+ElfType get_elf_type(uint16_t type);
+ElfArch get_elf_machine(uint16_t type);
 #endif
+
